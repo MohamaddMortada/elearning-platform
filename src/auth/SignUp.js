041d783563a55e25429a69be7d2e56449b53577e
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-21 16:51:13 +0200

    adding SignUp.js

---
+++ src/auth/SignUp.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { signup } from "../auth.js";
+
+const SignUp = () => {
+  const [name, setname] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      const token = await signup(name, password);
+      alert("Signup successful!");
+      console.log(token);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSignup}>
+      <h1>SignUp</h1>
+      <input
+        type="text"
+        placeholder="name"
+        value={name}
+        onChange={(e) => setname(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">SignUp</button>
+    </form>
+  );
+};
+
+export default SignUp;
